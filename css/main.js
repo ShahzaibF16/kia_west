8508c41564d12d13f9376d71e1afc18011f8130a
ShahzaibF16 <shahzaib1638@gmail.com>
2022-12-13 19:14:29 +0500

    Test Drive update

--- css/main.js
+++ css/main.js
@@ -38,18 +38,4 @@ thumbnails[i].addEventListener('mouseout', function() {
 }
 
 
-var state = document.getElementById("state")
-var city = document.getElementById("city")
-var dealer = document.getElementById("dealer")
-
-var state_List =  ["Gujarat"]
-var city_List = ["Ahmedabad", "Himmatnagar"]
-var ahmed_dealer = ["West Coast,SG Highway North","West Coast,Ambawadi"]
-var himmat_dealer = ["West Coast,Village motipura"]
-
-if (state.value == state_List[0]){
-    for(var i of city_List){
-        city.innerHTML += `<option>${i}</option>` 
-    }
-}
 
