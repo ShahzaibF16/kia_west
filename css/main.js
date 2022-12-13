4f4778d5fbd9f9d95bccfd124ce5774796762eed
ShahzaibF16 <shahzaib1638@gmail.com>
2022-12-13 18:26:35 +0500

    aster

--- css/main.js
+++ css/main.js
@@ -36,3 +36,20 @@ thumbnails[i].addEventListener('mouseout', function() {
     return play = setInterval(autoPlay, 20);
 });
 }
+
+
+var state = document.getElementById("state")
+var city = document.getElementById("city")
+var dealer = document.getElementById("dealer")
+
+var state_List =  ["Gujarat"]
+var city_List = ["Ahmedabad", "Himmatnagar"]
+var ahmed_dealer = ["West Coast,SG Highway North","West Coast,Ambawadi"]
+var himmat_dealer = ["West Coast,Village motipura"]
+
+if (state.value == state_List[0]){
+    for(var i of city_List){
+        city.innerHTML += `<option>${i}</option>` 
+    }
+}
+
